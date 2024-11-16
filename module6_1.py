eb2b6304d8ff3a26fd8cacf942a274822d0f34af
Sonitarach <sofar07@bk.ru>
2024-11-16 20:50:44 +0600

    Update module6_1.py

--- module6_1.py
+++ module6_1.py
@@ -1,36 +1,40 @@
 class Animal:
-    alive = True
-    fed = False
     def __init__(self, name):
+        self.alive = True
+        self.fed = False
+        self.name = name
+
+class Plant:
+    def __init__(self, name):
+        self.edible = False
         self.name = name
 
 class Mammal(Animal):
     def eat(self, food):
-        if Fruit.edible == True:
-            print(f'{self.name} съел {food.name}')
-            Animal.fed = True
+        if food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
         else:
-            print(f'{self.name} не стал есть {food.name}')
-            Animal.alive = False
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
 
 class Predator(Animal):
     def eat(self, food):
-        if Flower.edible == True:
-            print(f'{self.name} съел {food.name}')
-            Animal.fed = True
+        if food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
         else:
-            print(f'{self.name} не стал есть {food.name}')
-            Animal.alive = False
-
-class Plant:
-    edible = False
-    def __init__(self, name):
-        self.name = name
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
 
 class Flower(Plant):
-    edible = False
+    pass
+
 class Fruit(Plant):
-    edible = True
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = True
+
 
 a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
